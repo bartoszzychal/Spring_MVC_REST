4dc9686273ba65dccf36da2083af199fa13bb14a
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-02-26 15:16:48 +0100

    Added equals and hashcode

--- spring-data-model/src/main/java/pl/spring/demo/entity/BookEntity.java
+++ spring-data-model/src/main/java/pl/spring/demo/entity/BookEntity.java
@@ -47,4 +47,43 @@ public class BookEntity implements Serializable {
     public void setAuthors(String authors) {
         this.authors = authors;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((authors == null) ? 0 : authors.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		BookEntity other = (BookEntity) obj;
+		if (authors == null) {
+			if (other.authors != null)
+				return false;
+		} else if (!authors.equals(other.authors))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+    
+    
 }
