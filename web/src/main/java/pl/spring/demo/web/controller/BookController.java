7c4217640b7b9558340be946ede44ed0f2ef0242
Bartosz Zychal <bartosz.zychal@capgemini.com>
2016-03-01 15:50:50 +0100

    Add seraching by titile and aurhors mapper

--- web/src/main/java/pl/spring/demo/web/controller/BookController.java
+++ web/src/main/java/pl/spring/demo/web/controller/BookController.java
@@ -5,9 +5,11 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import pl.spring.demo.service.BookService;
 import pl.spring.demo.to.BookTo;
@@ -18,11 +20,31 @@ public class BookController {
     private BookService bookService;
 
     @RequestMapping(value = "/books", method = RequestMethod.GET)
-    public String bookList(Map<String, Object> params) {
+    public String bookList(Map<String, Object> params,
+    		@ModelAttribute("title") String title,
+    		@ModelAttribute("authors") String authors) {
         final List<BookTo> allBooks = bookService.findAllBooks();
         params.put("books", allBooks);
         return "bookList";
     }
+    
+    @RequestMapping(value = "/books", method = RequestMethod.POST)
+    public String bookListByTitle(Map<String, Object> params,
+    		@ModelAttribute("title") String title,
+    		@ModelAttribute("authors") String authors) {
+    	List<BookTo> bookByTitle= null;
+    	if(!title.isEmpty()&&!authors.isEmpty()){
+    		  bookByTitle = bookService.findBooksByTitleAndByAuthors(title,authors);
+    	}else if(!title.isEmpty()){
+  		  bookByTitle = bookService.findBooksByTitle(title);
+    	}else if(!authors.isEmpty()){
+    		bookByTitle = bookService.findBooksByAuthor(authors);
+    	}
+    	params.put("books", bookByTitle);
+    	return "bookList";
+    	
+    }
+    
     @RequestMapping(value = "/deleteBook/{id}", method = RequestMethod.GET)
     public String deleteBook(Map<String, Object> parameters,@PathVariable("id") String id) {
     	BookTo deleteBook = bookService.deleteBook(Long.valueOf(id));
